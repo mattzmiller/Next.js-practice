3efbb3f31e2dd8c8d2d7310f72ecb85a52d35791
mzecic <matejzecic@gmail.com>
2023-01-03 09:33:24 -0500

    initial commit

---
+++ pages/events/Event1.jsx
@@ -0,0 +1,5 @@
+export default function Event1() {
+    return (
+        <h1>Event Page</h1>
+    )
+}
---
+++ pages/events/index.jsx
@@ -0,0 +1,5 @@
+export default function index() {
+    return (
+        <h1>Next.js routing is awesome!</h1>
+    )
+}
